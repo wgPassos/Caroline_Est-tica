6259e3edb73ce5fee6453b193d1e411c2f687f64
wgPassos <passoswilliangabriel@gmail.com>
2021-06-18 19:04:58 -0300

    Foila

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let menu = document.querySelector('#menu');
 let navbar = document.querySelector('.navbar');
+let header = document.querySelector('header');
 
 menu.addEventListener('click', () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('nav-toggle');
+    header.classList.toggle('header-toggle');
 });
 
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('nav-toggle');
+    header.classList.remove('header-toggle')
 }
